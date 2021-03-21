276b47c8823fd9332948435566a8a5349c898f8e
Syndamia <kamen.d.mladenov@protonmail.com>
2021-03-21 18:27:14 +0200

    Implemented (moved from comment page) functionality for editing and deleting comments

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Guid } from 'guid-typescript';
 import { CommentService } from 'src/app/services/comment.service';
+import { TokenService } from 'src/app/services/token.service';
 import { UserService } from 'src/app/services/user.service';
 import { Comment } from 'src/models/comment.model';
 import { User } from 'src/models/identity/user.model';
@@ -13,12 +15,15 @@ import { User } from 'src/models/identity/user.model';
 })
 export class CommentComponent implements OnInit {
   public loaded = false;
+  public loggedInAuthor = false;
+  public editingComment = false;
   public user: User;
   public comment: Comment;
   public timeCreated: string;
   @Input() paramId: string;
+  public editCommentFormGroup: FormGroup;
 
-  constructor(private _router: Router, private _commentService: CommentService, private _userService: UserService)
+  constructor(private _router: Router, private _commentService: CommentService, private _userService: UserService, private _tokenService: TokenService, private _fb: FormBuilder)
   { }
 
   ngOnInit(): void {
@@ -33,12 +38,22 @@ export class CommentComponent implements OnInit {
         this.loadUser();
       }
     });
+
+    this.editCommentFormGroup = this._fb.group({
+      newCommentMessage: new FormControl('')
+    });
   }
 
   private loadUser(): void {
     this._userService.getUserByUsernameRequest(this.comment.issuerUsername).subscribe({
       next: (result: object) => {
         Object.assign(this.user, result);
+
+        if (this._tokenService.getTokenFromSessionStorage() !== '') {
+          this.loggedInAuthor = this._tokenService.getUsernameFromSessionStorageToken() === this.comment.issuerUsername;
+          this.editCommentFormGroup.get('newCommentMessage')?.setValue(this.comment.message);
+        }
+
         this.loaded = true;
       }
     });
@@ -46,5 +61,43 @@ export class CommentComponent implements OnInit {
 
   goToAuthorProfile(): void {
     this._router.navigate(['/profile/' + this.comment.issuerUsername]);
+  }
+
+  toggleEditing(): void {
+    this.editingComment = !this.editingComment;
+  }
+
+  editComment(): void {
+    if (this._tokenService.getTokenFromSessionStorage() === '') {
+      this._router.navigate(['/login']);
+      return;
+    }
+
+    if (this.editingComment) {
+      const newMessage = this.editCommentFormGroup.get('newCommentMessage')?.value;
+
+      if (newMessage !== '' && newMessage !== this.comment.message) {
+        this._commentService.putCommentWithSessionStorageRequest(Guid.parse(this.paramId), this.comment.postId, newMessage).subscribe({
+          next: () => {
+            this.reloadPage();
+          }
+        });
+      }
+    }
+    this.editingComment = !this.editingComment;
+  }
+
+  deleteComment(): void {
+    this._commentService.deleteCommentWithSessionStorage(Guid.parse(this.paramId)).subscribe({
+      next: () => {
+        this.reloadPage();
+      }
+    });
+  }
+
+  private reloadPage(): void {
+    this._router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this._router.onSameUrlNavigation = 'reload';
+    this._router.navigate([this._router.url]);
   }
 }
